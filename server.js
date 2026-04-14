ae82a84359e9eedef60b89abc23657d65c15008a
vijay <vijayg954@gmail.com>
2026-04-14 12:46:44 +0530

    first commit

--- server.js
+++ server.js
@@ -24,12 +24,13 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/test", (req, res) => {
   res.send("Test route working");
 });
-app.use("/api/v1/forestguard/quiz", quizRoutes);
+// app.use("/api/v1/forestguard/quiz", quizRoutes);
 app.use("/api/v1/forestguardhindi/quiz", quizRoutes);
 app.use("/api/v1/forestguardscience/quiz", quizRoutes);
 app.use("/api/v1/forestguardeng/quiz", quizRoutes);
 app.use("/api/v1/forestguardmaths/quiz", quizRoutes);
 app.use("/api/v1/forestguardmpgk/quiz", quizRoutes);
+app.use("/api/v1/forestpractice/quiz", quizRoutes);
 
 app.listen(PORT, () => {
   console.log(`App is listening on ${PORT}`);
